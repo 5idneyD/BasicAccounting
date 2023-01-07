5cc9176d4b8d33c2a592ea51dba4ed4510c99ec1
5idneyD <sldriscoll@btinternet.com>
2023-01-07 18:41:26 +0000

    Can sign-up, add users via admin page and login

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_migrate import Migrate
 import os
 from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
 
 app = Flask(__name__)
 
@@ -13,10 +15,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + \
     os.path.join(basedir, "./database/database.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=1)
 app.config['SECRET_KEY'] = os.urandom(12).hex()
 
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Companies(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
@@ -29,11 +33,17 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     company = db.Column(db.String(40))
     username = db.Column(db.String(40))
+    email = db.Column(db.String(40))
     password = db.Column(db.String(40))
     admin = db.Column(db.Boolean)
 
 with app.app_context():
     db.create_all()
+
+@app.before_request
+def before_request():
+    # session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=1)
 
 @app.route("/")
 def index():
@@ -46,11 +56,16 @@ def login():
         email = request.form["email"]
         password = request.form["password"]
 
-        users = db.session.execute("SELECT * FROM Users WHERE username='" + email + "';")
+        users = db.session.execute("SELECT * FROM Users WHERE email='" + email + "';")
         # each user contains id, company, email & password
         for user in users:
+            print(user)
             if user["password"] == password:
-                return redirect(f"/{user['company']}/dashboard/{user['username']}")
+                if user["admin"] == True:
+                    return redirect(f"/admin/{user['company']}")
+                else:
+                    session_key = app.config['SECRET_KEY']
+                    return redirect(f"/{user['company']}/dashboard/{user['username']}/{session_key}")
         # print(users)
 
     return render_template("login.html")
@@ -80,10 +95,11 @@ def signup():
 def admin(company):
 
     if request.method == "POST":
+        new_name = request.form["name"]
         new_email = request.form["email"]
         new_password = request.form["password"]
 
-        new_user = Users(company=company, username=new_email,
+        new_user = Users(company=company, username=new_name, email=new_email,
                          password=new_password, admin=False)
         db.session.add(new_user)
         db.session.commit()
@@ -92,10 +108,14 @@ def admin(company):
     return render_template("admin.html", company=company)
 
 
-@app.route("/<company>/dashboard/<username>")
-def dashboard(company, username):
+@app.route("/<company>/dashboard/<username>/<key>", methods=["POST", "GET"])
+def dashboard(company, username, key):
 
-    return render_template("dashboard.html", company=company, username=username)
+    # print(app.config['SECRET_KEY'])
+    if key == app.config['SECRET_KEY']:
+        return render_template("dashboard.html", company=company, username=username)
+    else:
+        return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
