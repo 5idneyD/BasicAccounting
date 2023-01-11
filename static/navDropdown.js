4d0e4ebc5063608f62690941decb6418ca975050
5idneyD <sldriscoll@btinternet.com>
2023-01-11 10:49:30 +0000

    Can jump between pages whilst keeping url data

--- static/navDropdown.js
+++ static/navDropdown.js
@@ -2,7 +2,8 @@ document.addEventListener(
     "click",
     function (e) {
         var menu = document.querySelector("#" + e.target.id);
-
+        var url = window.location.pathname;
+        url = url.substring(0, url.lastIndexOf("/"));
 
 
         if (menu.dataset.expanded == "false") {
@@ -11,42 +12,42 @@ document.addEventListener(
                 menu.insertAdjacentHTML(
                     "beforeend",
                     "<br class=" +
-                        e.target.id +
-                        "Option><a class='menuOption " +
-                        e.target.id +
-                        "Option'>Add Sales Invoice</a><br class=" +
-                        e.target.id +
-                        "Option><a class='menuOption " +
-                        e.target.id +
-                        "Option'>View Customers</a>",
+                    e.target.id +
+                    "Option><a class='menuOption " +
+                    e.target.id +
+                    "Option'>Add Sales Invoice</a><br class=" +
+                    e.target.id +
+                    "Option><a class='menuOption " +
+                    e.target.id +
+                    "Option'>View Customers</a>",
                 );
                 menu.dataset.expanded = "true";
             } else if (menu.id == "purchaseLedger") {
                 menu.insertAdjacentHTML(
                     "beforeend",
                     "<br class=" +
-                        e.target.id +
-                        "Option><a class='menuOption " +
-                        e.target.id +
-                        "Option'>Add Purchase Invoice</a><br class=" +
-                        e.target.id +
-                        "Option><a class='menuOption " +
-                        e.target.id +
-                        "Option'>View Suppliers</a>",
+                    e.target.id +
+                    "Option><a class='menuOption " +
+                    e.target.id +
+                    "Option'>Add Purchase Invoice</a><br class=" +
+                    e.target.id +
+                    "Option><a class='menuOption " +
+                    e.target.id +
+                    "Option'>View Suppliers</a>",
                 );
                 menu.dataset.expanded = "true";
             } else {
                 menu.insertAdjacentHTML(
                     "beforeend",
                     "<br class=" +
-                        e.target.id +
-                        "Option><a class='menuOption " +
-                        e.target.id +
-                        "Option'>Trial Balance</a><br class=" +
-                        e.target.id +
-                        "Option><a class='menuOption " +
-                        e.target.id +
-                        "Option'>Chart Of Accounts</a>",
+                    e.target.id +
+                    "Option><a class='menuOption " +
+                    e.target.id +
+                    "Option'>Trial Balance</a><br class=" +
+                    e.target.id +
+                    "Option><a class='menuOption " +
+                    e.target.id +
+                    "Option' href='" + url + "/chartOfAccounts'>Chart Of Accounts</a>",
                 );
                 menu.dataset.expanded = "true";
             }
