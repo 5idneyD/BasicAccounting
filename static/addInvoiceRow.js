9bc51122f9f7519a27fffca239af121c1fff6484
5idneyD <sldriscoll@btinternet.com>
2023-01-15 15:21:13 +0000

    Sales Invoice calculates VAT & Total automatically

--- static/addInvoiceRow.js
+++ static/addInvoiceRow.js
@@ -10,9 +10,9 @@ function addInvoiceRow() {
             <td>` + row_number + `
             <td><input type="text" name='` + row_number + `_nominal_code'></td>
             <td><input type="text" name='` + row_number + `_description'></td>
-            <td><input type="text" name='` + row_number + `_net_value'></td>
-            <td><input type="text" name='` + row_number + `_vat'></td>
-            <td><input type="text" name='` + row_number + `_total_value'></td>
+            <td><input type="text" name='` + row_number + `_net_value' class='net'></td>
+            <td><input type="text" name='` + row_number + `_vat' class='vat'></td>
+            <td><input type="text" name='` + row_number + `_total_value' class='tv'></td>
         </tr>
  `
     row_number += 1;
---
+++ static/defaultSalesInvoiceNumber.js
@@ -0,0 +1,20 @@
+var references = document.querySelector("#references").dataset.references;
+var company = document.querySelector("#references").dataset.company;
+var invoice_number_element = document.querySelector("#invoice_number");
+var submitButton = document.querySelector("#submitButton");
+
+var new_number = Number(references[references.length - 3]) + 1;
+invoice_number_element.value = new_number;
+
+invoice_number_element.addEventListener("change", function () {
+    var new_reference = company + invoice_number_element.value;
+    if (references.includes(new_reference)) {
+        invoice_number_element.style.border = "1px solid red";
+        submitButton.setAttribute("disabled", "disabled")
+        console.log("Used");
+    } else {
+        console.log("Unused");
+        invoice_number_element.style.border = "1px solid gray";
+        submitButton.removeAttribute("disabled")
+    }
+});
