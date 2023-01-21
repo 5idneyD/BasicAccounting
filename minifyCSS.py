de75e030175e4195cf8c89210ca836479506c30c
5idneyD <sldriscoll@btinternet.com>
2023-01-21 15:29:36 +0000

    Adding some animation to the home page

--- minifyCSS.py
+++ minifyCSS.py
@@ -2,12 +2,12 @@ import cssmin
 import sys
 
 def minify(file):
-    output = cssmin.cssmin(open("./static/" + file).read())
+    output = cssmin.cssmin(open("./static/styles/" + file).read())
     # print(output)
-    with open("./static/" + file[0:file.index(".")]+".min.css", "w") as f:
+    with open("./static/styles/" + file[0:file.index(".")]+".min.css", "w") as f:
         # print("./static/" + file[0:file.index(".")]+".min.css", "w")
         f.write(output)
 
 minify(sys.argv[1])
-print("./static/" + sys.argv[1][0:sys.argv[1].index(".")]+".min.css" + " produced")
+print("./static/styles/" + sys.argv[1][0:sys.argv[1].index(".")]+".min.css" + " produced")
 
